b5237c37aac5a39cf20f64f6c9b7d5ab7a073c41
Peonat <duytan2804@gmail.com>
2022-02-20 20:27:35 +0700

    Revert "Data persistence with JPA"
    This reverts commit 6bd3c85d8d224a7e4db4d53e6843e720facdf4c8.

--- src/main/java/sia/tacocloud/web/DesignTacoController.java
+++ src/main/java/sia/tacocloud/web/DesignTacoController.java
@@ -1,54 +1,23 @@
 package sia.tacocloud.web;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 import sia.tacocloud.Ingredient;
-import sia.tacocloud.Order;
 import sia.tacocloud.Taco;
 import sia.tacocloud.Ingredient.Type;
-import sia.tacocloud.data.IngredientRepository;
-import sia.tacocloud.data.TacoRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
-@SessionAttributes("order")
 public class DesignTacoController {
-    private final IngredientRepository ingredientRepo;
-    private final TacoRepository tacoRepository;
-
-    @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepo,
-                                TacoRepository tacoRepository) {
-        this.ingredientRepo = ingredientRepo;
-        this.tacoRepository = tacoRepository;
-    }
-
-    @ModelAttribute(name = "order")
-    public Order order() {
-        return new Order();
-    }
-
-    @ModelAttribute(name = "taco")
-    public Taco taco() {
-        return new Taco();
-    }
-
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream()
                 .filter(x -> x.getType().equals(type))
@@ -57,8 +26,18 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = new ArrayList<Ingredient>();
-        ingredientRepo.findAll().forEach((i) -> ingredients.add(i));
+        List<Ingredient> ingredients = Arrays.asList(
+            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+            new Ingredient("CHED", "Cheddar", Type.CHEESE),
+            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+            new Ingredient("SLSA", "Salsa", Type.SAUCE),
+            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
 
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
@@ -66,27 +45,7 @@ public class DesignTacoController {
                 filterByType(ingredients, type));
         }
 
+        model.addAttribute("design", new Taco());
         return "design";
-    }
-
-    @PostMapping
-    public String processDesign(Model model, @Valid Taco taco, Errors errors, @ModelAttribute Order order) {
-        if (errors.hasErrors()) {
-            log.info("Has some Error");
-            List<Ingredient> ingredients = new ArrayList<Ingredient>();
-            ingredientRepo.findAll().forEach((i) -> ingredients.add(i));
-    
-            Type[] types = Ingredient.Type.values();
-            for (Type type : types) {
-                model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
-            }
-            return "design";
-        }
-
-        Taco saved = tacoRepository.save(taco);
-        order.addDesign(saved);
-        log.info("Process design: " + taco);
-        return "redirect:/orders/current";
     }
 }
