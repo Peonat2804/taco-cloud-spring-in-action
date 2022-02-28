f8a6d6e853a2d77986e70efe836ba38cabe87672
Peonat <duytan2804@gmail.com>
2022-02-28 22:22:13 +0700

    Add user login details

---
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,59 @@
+package sia.tacocloud.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+/* In-Memory Authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("buzz")
+                    .password("{noop}infinity")
+                    .authorities("ROLE_USER")
+                .and()
+                .withUser("woody")
+                    .password("{noop}bullseye")
+                    .authorities("ROLE_USER");
+    }
+*/
+
+/* JDBC-based user store 
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .jdbcAuthentication()
+                .dataSource(dataSource);
+    }
+*/
+
+/* Customized log-in */
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(encoder());
+    }
+}
---
+++ src/main/java/sia/tacocloud/web/RegistrationController.java
@@ -0,0 +1,36 @@
+package sia.tacocloud.web;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sia.tacocloud.data.UserRepository;
+import sia.tacocloud.security.RegistrationForm;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form) {
+        userRepository.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
+    
+}
